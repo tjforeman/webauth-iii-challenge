7ba5a17de2d3fd7fd8e87c6c77d5e386aa9731d7
tjforeman <tylerforeman92@gmail.com>
2019-05-08 16:42:27 -0400

    register and login working correctly

--- routes/login-route.js
+++ routes/login-route.js
@@ -1,5 +1,36 @@
 const router = require('express').Router();
 const db =require('../data/dbConfig.js');
+const bcrypt=require('bcryptjs')
+const jwt =require('jsonwebtoken')
+const secrets=require('../config/secret.js')
 
+router.post('/', (req, res) => {
+    let {username,password}=req.body 
+    db('users')
+    .where({username})
+    .first()
+    .then(user =>{
+        if(user&& bcrypt.compareSync(password,user.password)){
+            const token=generateToken(user)
+        res.status(200).json({message:`Welcome ${user.username}, you have successfully logged in`,token})
+        }else{
+            res.status(401).json({message:'you shall not pass!'})
+        }
+        })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to log in at this time'})
+    })
+      });
+
+function generateToken(user){
+const payload={
+    subject: user.id,
+    username:user.username
+};
+const options={
+    expiresIn:'1h'
+};
+    return jwt.sign(payload, secrets.jwtSecret, options)
+}
 
 module.exports=router
--- routes/register-route.js
+++ routes/register-route.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
 const db =require('../data/dbConfig.js');
+const bcrypt = require('bcryptjs');
+
+router.post('/', (req, res) => {
+    let user=req.body 
+    const hash=bcrypt.hashSync(user.password,10)
+    user.password=hash
+    db('users')
+    .insert(req.body)
+    .then(user =>{
+        res.status(200).json(user)
+        })
+    .catch(err=>{
+        res.status(500).json({error:err,message:'Unable to sign up at this time'})
+    })
+      });
 
 
 module.exports=router
