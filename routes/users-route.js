8d9374cf6d4160ef69e7b17ae1cf0708b7f3ea25
tjforeman <tylerforeman92@gmail.com>
2019-05-08 17:21:04 -0400

    protected route working

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const db =require('../data/dbConfig.js');
+const protected=require('../protected/protected.js')
 
-router.get('/', (req, res) => {
+router.get('/', protected,(req, res) => {
     db('users')
       .then(users => {
         res.json(users);
